d438946d57c27fd02c3d4f83bfeca8831613e9bf
Luana Moraes <luanamoraes200030@gmail.com>
2023-05-12 21:01:25 -0300

    atualizacao

--- mr.donuts/server.js
+++ mr.donuts/server.js
@@ -1,22 +1,32 @@
+//Curso de Engenharia de Software - UniEVANGÉLICA 
+//Disciplina -  Programação Web 
+//Dev:  Luana Teixeira de Moraes 
+//Data: 12/05/23
+
 const express = require('express')
 const app = express()
 const port = 8080
 
-app.use(express.json())
+app.use(express.json()) //Importa o módulo 'express' e cria uma instância do aplicativo Express. Define a variável 'port' para armazenar o número da porta que o servidor irá escutar (porta 8080).//
 
-const donuts = [
+//Define o middleware 'express.json()', que permite que o servidor entenda os dados JSON enviados pelos clientes.//
+const donuts = [ 
   { id: 1, flavor: 'Chocolate' },
   { id: 2, flavor: 'Strawberry' },
 ]
 
+//Cria uma array 'donuts' com dois objetos que representam sabores de rosquinhas.//
 app.get('/', (req, res) => {
-  res.redirect('/donuts')
-})
+  res.redirect('/donuts') //
+}) 
+javascript
 
+//Define uma rota para a página inicial ('/'), que redireciona para a rota '/donuts'.
 app.get('/donuts', (req, res) => {
   res.json(donuts)
 })
 
+//Define uma rota para '/donuts', que retorna um objeto JSON contendo as informações das rosquinhas presentes na array 'donuts'.
 app.get('/donuts/:id', (req, res) => {
   const id = parseInt(req.params.id)
   const donut = donuts.find(donut => donut.id === id)
@@ -26,12 +36,14 @@ app.get('/donuts/:id', (req, res) => {
   res.json(donut)
 })
 
+//Define uma rota para '/donuts/:id', que recebe o ID da rosquinha como um parâmetro e retorna o objeto JSON correspondente a essa rosquinha. Se a rosquinha não existir, um erro 404 é retornado.
 app.post('/donuts', (req, res) => {
   const donut = req.body
   donuts.push(donut)
   res.json({ inputs: donut })
 })
 
+//Define uma rota para criar uma nova rosquinha, adicionando-a à array 'donuts'. A nova rosquinha é recebida como um objeto JSON no corpo da solicitação. O servidor retorna um objeto JSON contendo a nova rosquinha adicionada.
 app.put('/donuts/:id', (req, res) => {
   const id = parseInt(req.params.id)
   const donut = req.body
@@ -43,6 +55,7 @@ app.put('/donuts/:id', (req, res) => {
   res.json({ inputs: donuts[index] })
 })
 
+//Define uma rota para atualizar as informações de uma rosquinha existente. A rosquinha a ser atualizada é especificada pelo ID. A nova informação da rosquinha é recebida como um objeto JSON no corpo da solicitação. O servidor retorna um objeto JSON contendo a rosquinha atualizada.
 app.delete('/donuts/:id', (req, res) => {
   const id = parseInt(req.params.id)
   const index = donuts.findIndex(donut => donut.id === id)
